7735747d590244e19fe32ccad7853f891be78821
Mr_Vektor04 <56996845+Vektor04@users.noreply.github.com>
2019-10-27 14:40:30 +0000

    Update Kalkulator.py

--- Kalkulator.py
+++ Kalkulator.py
@@ -21,7 +21,7 @@ while lagi=='y':
     print ' \t \t ===================================== '
     a= input(' Masukan Bilangan 1: ')
     b= input (' Masukan Biilangan 2 : ')
-print ' 1. Penjumlahan \n 2. pengurangan \n 3. perkalian \n>
+    print ' 1. Penjumlahan \n 2. pengurangan \n 3. perkalian \n>
     c= input ('Pilih 1-5 : ')
     if c== 1:
         print ' Hasilnya adalah = ', penjumlahan (a,b)
